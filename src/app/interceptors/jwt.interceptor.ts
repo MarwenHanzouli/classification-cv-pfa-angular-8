87421d21cbe08531860d11574d1478b78009a662
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-23 21:14:45 +0100

    create token interceptor

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -3,12 +3,27 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
+import { AuthentificationService } from '../services/authentification.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private toastr: ToastrService) {}
+    constructor(private toastr: ToastrService,
+                private authService:AuthentificationService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      if (request.url.indexOf('authenticate') !== -1) {
+        console.log("pas de token à l'authentification");
+        return next.handle(request); // do nothing
+      }
+      let currentUser = this.authService.currentUserValue;
+      if (currentUser && currentUser.token) {
+        console.log("token---------------------");
+          request = request.clone({
+              setHeaders: { 
+                  Authorization: `Bearer ${currentUser.token}`
+              }
+          });
+      }
         // add authorization header with jwt token if available
         /*request = request.clone({
             setHeaders: { 
