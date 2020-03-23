768a2b909ad3e8cbe5fcc2d1156459b19f96483c
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-23 21:23:22 +0100

    add id and other attributes to model user

--- src/app/models/User.model.ts
+++ src/app/models/User.model.ts
@@ -10,11 +10,17 @@ export class User{
         public lastName: string,
         public telephone:string,
         public roleName:string,
+        public id?:number,
+        public active?:number,
+        public isLoacked?:boolean,
+        public isExpired?:boolean,
+        public isEnabled?:boolean,
         public address?: Address,
         public niveau?:string,
         public diplome?:string,
         public institut?:string,
         public date_naissance?:string,
-        public nameEntreprise?:string
+        public nameEntreprise?:string,
+        public token?:string
     ){}
 }
