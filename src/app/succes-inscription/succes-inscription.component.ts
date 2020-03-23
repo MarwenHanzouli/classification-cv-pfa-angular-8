0d8f38dabe5a8e9762e8a41845dde27e7c9606d7
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-23 02:00:45 +0100

    add succes inscription component

---
+++ src/app/succes-inscription/succes-inscription.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-succes-inscription',
+  templateUrl: './succes-inscription.component.html',
+  styleUrls: ['./succes-inscription.component.css']
+})
+export class SuccesInscriptionComponent implements OnInit {
+
+  @Input() email:string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
