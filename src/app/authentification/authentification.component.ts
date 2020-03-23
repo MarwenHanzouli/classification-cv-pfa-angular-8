e5bbec5afa3b71a2014d4d64510ce6ec60292b9d
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-23 21:28:12 +0100

    update authentiation component and service

--- src/app/authentification/authentification.component.ts
+++ src/app/authentification/authentification.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthentificationService } from '../services/authentification.service';
+import { ToastrService } from 'ngx-toastr';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-authentification',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthentificationComponent implements OnInit {
 
-  constructor() { }
+  authForm: FormGroup;
+  submitted: boolean=false;
+  loading:boolean=false;
+  autenticated:boolean=false;
+  error:boolean=false;
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthentificationService,
+              private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.initForm();
   }
+  initForm()
+  {
+    this.authForm = this.formBuilder.group(
+      {
+        emailOrUsername: ['', Validators.required],
+        password: ['', Validators.required]
+      }
+    );
+  }
+  get f() { return this.authForm.controls; }
 
+  onSubmitForm(){
+    this.submitted=true;
+    if(this.authForm.invalid)
+    {
+      return ;
+    }
+    let user={
+      "username":this.authForm.value["emailOrUsername"],
+      "password":this.authForm.value["password"]
+    }
+    console.log(user);
+
+    this.loading=true;
+    this.authService.login(user).pipe(first()).subscribe((response)=>{
+      console.log(response);
+      this.error=false;
+    },
+    (error)=>{
+      if(error['status']==401)
+      {
+        this.error=true;
+        this.toastr.error("Ces informations ne correspondent à aucun compte","Erreur de connexion");
+      }
+      console.log(error);
+    });
+    
+    this.loading=false;
+  }
 }
--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/User.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
 
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private httpClient: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+   }
+
+  
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+
+  login(user):Observable<any>{
+    return this.httpClient.post('http://127.0.0.1:9004/authenticate',user, 
+    {headers: this.headers}).pipe(map(response => {
+      // login successful if there's a jwt token in the response
+      if (response && response['token']) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          this.currentUserSubject.next(response['user']);
+      }
+
+      return user;
+  }));;
+  }
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+}
+
 }
