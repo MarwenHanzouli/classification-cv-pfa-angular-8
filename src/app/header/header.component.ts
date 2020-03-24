bb739cb3ae8da4310dbd6a29026e4ce953a6d266
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-24 19:54:31 +0100

    update header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt, faUserPlus, faSignOutAlt, faUserCircle, faBell} from '@fortawesome/free-solid-svg-icons';
+import { User } from '../models/User.model';
+import { Subscription } from 'rxjs';
+import { AuthentificationService } from '../services/authentification.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,9 +12,35 @@ import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent implements OnInit {
   faSignInAlt = faSignInAlt;
   faUserPlus = faUserPlus;
-  constructor() { }
+  faSignOutAlt= faSignOutAlt;
+  faUserCircle= faUserCircle;
+  faBell= faBell;
+
+  private connected:boolean=false;
+  private currentUser: User;
+  private currentUserSubscription: Subscription;
+
+  constructor(private authService:AuthentificationService,
+              private router: Router) { 
+    this.currentUserSubscription=this.authService.currentUser.subscribe(
+      (user)=>{
+        if(user)
+        {
+          this.currentUser=user;
+          console.log(user);
+          this.connected=true;
+        }
+        else {
+          this.connected=false;
+        }
+    });
+  }
 
   ngOnInit() {
   }
 
+  logout(){
+    this.authService.logout();
+    this.router.navigate(["/authentification"]);
+  }
 }
