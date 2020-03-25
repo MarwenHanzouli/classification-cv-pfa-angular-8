6a65924a5c62ebd895775edcb4accc1977a30eb8
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-25 17:47:31 +0100

    move accueil component to module of candidat

---
+++ src/app/candidat/accueil-candidat/accueil-candidat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-candidat',
+  templateUrl: './accueil-candidat.component.html',
+  styleUrls: ['./accueil-candidat.component.css']
+})
+export class AccueilCandidatComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
