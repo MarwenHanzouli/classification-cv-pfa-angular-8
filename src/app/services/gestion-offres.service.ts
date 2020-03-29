587f232515898b719910d4f9d305a27371fa1a52
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-29 20:31:02 +0100

    add offres service

---
+++ src/app/services/gestion-offres.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Offre } from '../models/Offre.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class GestionOffresService {
+
+  offresSubject:BehaviorSubject<Offre[]>;
+
+  private offres:Offre[];
+  constructor() { 
+    this.offres=[
+      {"id":1,"titre":'Développeur JAVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":2,"titre":'Développeur C#',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":3,"titre":'Développeur sdfsdf',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":4,"titre":'Développeur vxcsdf',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":5,"titre":'Développeur bdgfv',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":6,"titre":'Développeur zerfzer',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":7,"titre":'Développeur JAqsdfqVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":8,"titre":'sqdfxcw sdc',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":9,"titre":'qscdwxc JAVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":10,"titre":'vvc JAVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":11,"titre":'wxcvxcv JAVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":12,"titre":'esvd JAVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":13,"titre":'efs ssdf',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":14,"titre":'sfsd sc ',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":15,"titre":'sdf JAVsdfA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":16,"titre":'dv JAVsdvA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":17,"titre":'vsdv JAVA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":18,"titre":'sff JAVAsdfgs',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":19,"titre":'Dévelovcxsfvcvppeur fdg',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"},
+      {"id":20,"titre":'sfdgfg JAVsfgsfA',"dateOffre":new Date("2020/12/20"),"entreprise":"Poulina"}
+    ];
+    this.offresSubject=new BehaviorSubject(this.offres);
+  }
+}
