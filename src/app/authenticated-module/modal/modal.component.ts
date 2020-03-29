a094e9a72fff8251b3d2fc8e1a39661d909f572e
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-29 17:08:41 +0100

    create modal component

--- src/app/authenticated-module/modal/modal.component.ts
+++ src/app/authenticated-module/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChildren, QueryList, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() titre:string;
+  @Input() tableauClasses:boolean[];
+  private classStyle;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.classStyle={
+      'modal-dialog-centered':this.tableauClasses[0],
+      'modal-sm':this.tableauClasses[1],
+      'modal-lg':this.tableauClasses[2],
+      'modal-xl':this.tableauClasses[3]
+    }
   }
 
 }
